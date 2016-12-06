bcc65613e855394d8dc9724273eb134fdb07e1f3
krasnirex <krasnirex@gmail.com>
2016-12-06 16:24:55 -0500

    Update color.js

--- color.js
+++ color.js
@@ -8,11 +8,13 @@
         if (clicked)
         {
             Entities.editEntity(entityID, { color: { red: 0, green: 0, blue: 255} });
+            Entities.editEntity(entityID, { gravity: { x: 0, y: 0, z: 9} });
             clicked = false;
         }
         else
         {
             Entities.editEntity(entityID, { color: { red: 0, green: 0, blue: 0} });
+            Entities.editEntity(entityID, { gravity: { x: 0, y: 0, z: 0} });
             clicked = true; 
         }
     }; 
