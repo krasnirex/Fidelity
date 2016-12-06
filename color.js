028fda3f114d34897d48f3b03da99ac0d980a8f6
krasnirex <krasnirex@gmail.com>
2016-12-06 15:25:58 -0500

    Update color.js

--- color.js
+++ color.js
@@ -3,10 +3,10 @@
     var clicked = false;
     this.clickDownOnEntity = function(entityID, mouseEvent) { 
         if (clicked){
-            Entities.editEntity(entityID, { color: { red: 0, green: 255, blue: 255} });
+            Entities.editEntity(entityID, { color: { red: 0, green: 0, blue: 255} });
             clicked = false;
         }else{
-            Entities.editEntity(entityID, { color: { red: 255, green: 255, blue: 0} });
+            Entities.editEntity(entityID, { color: { red: 255, green: 0, blue: 0} });
             clicked = true; 
         }
     }; 
